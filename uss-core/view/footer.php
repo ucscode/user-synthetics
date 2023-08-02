114547d73b8acd24d1274038616d18d4e70dc695
ucscode <uche23mail@gmail.com>
2023-08-02 06:42:10 +0100

    Made code more readable

--- uss-core/view/footer.php
+++ uss-core/view/footer.php
@@ -1,18 +1,18 @@
-<?php
+	<?php
 
-	# Avoid Error Display
-	defined('ROOT_DIR') or die;
+		# Avoid Error Display
+		defined('ROOT_DIR') or die;
 
-	# Before Default Footer Scripts
-	Events::exec("@body:beforeAfter");
+		# Before Default Footer Scripts
+		Events::exec("@body:beforeAfter");
 
-	# The Footer Scripts
-	echo self::include_libraries('body', $exclude_libraries) . "\n"; 
-	
-	# After Default Footer Scripts
-	Events::exec('@body:after'); 
+		# The Footer Scripts
+		echo self::include_libraries('body', $exclude_libraries) . "\n"; 
+		
+		# After Default Footer Scripts
+		Events::exec('@body:after'); 
 
-?>
+	?>
 		
 </body>
 </html>
