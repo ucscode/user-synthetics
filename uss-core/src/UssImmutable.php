697e4c8e752f47f0ffc634d4f49ebceb1bebffba
ucscode <uche23mail@gmail.com>
2023-10-13 01:26:59 +0100

    v3.3.4 | refactor

--- uss-core/src/UssImmutable.php
+++ uss-core/src/UssImmutable.php
@@ -3,34 +3,19 @@
 final class UssImmutable
 {
     public const DEBUG = true;
-
     public const SECRET_KEY = 'any_secret_key';
-
     public const PROJECT_NAME = 'User Synthetics';
-
     public const GITHUB_REPO = 'https://github.com/ucscode/user-synthetics';
-
     public const AUTHOR = 'Ucscode';
-
     public const AUTHOR_EMAIL = 'uche23mail@gmail.com';
-
     public const AUTHOR_WEBSITE = 'https://ucscode.me';
-
     public const MIN_PHP_VERSION = '8.1';
-
     public const CORE_DIR = ROOT_DIR . "/uss-core";
-
     public const MOD_DIR = ROOT_DIR . '/uss-modules';
-
     public const VENDOR_DIR = ROOT_DIR . '/vendor';
-
     public const ASSETS_DIR = self::CORE_DIR . '/assets';
-
     public const VIEW_DIR = self::CORE_DIR . '/view';
-
     public const SRC_DIR = self::CORE_DIR . "/src";
-
     public const CONFIG_DIR = self::CORE_DIR . "/config";
-
     public const JSON_DIR = self::ASSETS_DIR . '/JSON';
 }
