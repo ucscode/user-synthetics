39902710e54642c31c315f136f6de54bee9ae9a1
ucscode <uche23mail@gmail.com>
2023-12-04 07:24:42 +0100

    v4.1.0 | style: Added docblock info about project in index.php file

--- uss-core/index.php
+++ uss-core/index.php
@@ -1,11 +1,30 @@
 <?php
 /**
- * index.php - Project Entry Point
+ * User Synthetics Framework
  *
- * This file serves as the entry point for the project. It defines the 'ROOT_DIR'
- * constant, includes necessary files for the project, and initializes key components.
+ * A versatile PHP framework designed for the seamless development of innovative projects.
  *
- * @package Ucscode\Uss
+ * Combining the flexibility of a framework and the feature-rich capabilities of a CMS, User Synthetic empowers
+ * developers to build and customize applications efficiently. With a focus on simplicity and extensibility,
+ * it provides a foundation for creating diverse web applications and platforms.
+ *
+ * @category   Framework
+ * @package    Ucscode\Uss
+ * @author     Uchenna Ajah
+ * @license    MIT License
+ * @link       https://github.com/ucscode/user-synthetics
+ * @version    4.1.0
+ *
+ * Requirements:
+ * - PHP 8.1+
+ * - Composer for dependency management
+ *
+ * Directory Structure:
+ * - /uss-core: Contains the project source code
+ * - /uss-modules: Contains modules for platform expansion
+ * - /vendor: Composer dependencies
+ *
+ * @see https://github.com/ucscode/user-synthetics
  */
 
 // Define the root directory for the project
