a28baf2222406089eb2de566aa335f8feb88d902
ucscode <uche23mail@gmail.com>
2023-12-23 13:54:47 +0100

    Removed preserved directories

--- modules/Shares/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($fqcn) {
-    $fqcn = explode("\\", $fqcn);
-    array_shift($fqcn);
-    $classFile = __DIR__ . "/resource/" . implode("/", $fqcn) . ".php";
-    require_once $classFile;
-});
