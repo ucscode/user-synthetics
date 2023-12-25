6f181b448e25adde73ebbdaa4aa6151a0f0f93e0
ucscode <uche23mail@gmail.com>
2023-12-25 13:17:36 +0100

    v5.2.1 | feat(Doc): Added docblock for SQueryTrait

--- cores/Packages/SQuery/SQueryTrait.php
+++ cores/Packages/SQuery/SQueryTrait.php
@@ -4,6 +4,11 @@ namespace Ucscode\SQuery;
 
 trait SQueryTrait
 {
+    /**
+     * Backtick a string representing an SQL column or reserved word
+     * 
+     * @param string $entity - The column or reserved word to backtick
+     */
     protected function tick(string $entity): string
     {
         $entity = array_map(function($value) {
@@ -14,6 +19,12 @@ trait SQueryTrait
         return implode(".", $entity);
     }
 
+    /**
+     * Surround any text with the specified character
+     * 
+     * @param string $value - The text to be surrounded
+     * @param string $char - The character used to surround the text
+     */
     protected function surround(string $value, string $char): string
     {
         $pattern = sprintf('/^%s.*%s$/', $char, $char);
