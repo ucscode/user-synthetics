9467e449500be136b8f29ae18e017daca6251778
ucscode <uche23mail@gmail.com>
2024-01-04 05:19:33 +0100

    v5.5.1 | feat: Added Security Key Compression With Openssl Encryption

--- cores/Packages/LocalStorage/Abstract/AbstractLocalStorage.php
+++ cores/Packages/LocalStorage/Abstract/AbstractLocalStorage.php
@@ -8,13 +8,15 @@ use Ucscode\LocalStorage\Interface\LocalStorageInterface;
 
 abstract class AbstractLocalStorage implements LocalStorageInterface
 {
+    protected const ALGORITHM = 'aes-256-cbc';
+    protected string $iv;
     protected array $storage;
 
-    public function __construct(protected string $filepath)
+    public function __construct(protected string $filepath, protected ?string $secretKey = null)
     {
+        $this->iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::ALGORITHM));
         $content = file_get_contents($filepath);
-        $this->storage = (is_string($content) && !empty($content)) ? ($this->decode($content) ?: []) : [];
-        var_dump($this->storage);
+        $this->storage = (!empty($content)) ? ($this->decode($content) ?: []) : [];
     }
 
     public function &__get(string $offset): mixed
@@ -45,7 +47,7 @@ abstract class AbstractLocalStorage implements LocalStorageInterface
 
     protected function encode(): string
     {
-        return 
+        $encoding = 
             gzdeflate(
                 bin2hex(
                     str_rot13(
@@ -56,13 +58,48 @@ abstract class AbstractLocalStorage implements LocalStorageInterface
                         )
                     )
                 )
-            )
-        ;
+            );
+
+        if(!empty($this->secretKey)) 
+        {
+            $encryption = openssl_encrypt(
+                $encoding,
+                self::ALGORITHM,
+                $this->secretKey,
+                OPENSSL_RAW_DATA,
+                $this->iv
+            );
+
+            $encoding = $this->iv . $encryption;
+        }
+
+        return $encoding;
     }
 
     protected function decode(string $encoding): bool|array
     {
+        if(!empty($this->secretKey)) 
+        {
+            $IV = substr($encoding, 0, openssl_cipher_iv_length(self::ALGORITHM));
+            $encryption = substr($encoding, openssl_cipher_iv_length(self::ALGORITHM));
+
+            $encoding = openssl_decrypt(
+                $encryption,
+                self::ALGORITHM,
+                $this->secretKey,
+                OPENSSL_RAW_DATA,
+                $IV
+            );
+            
+            if($encoding === false) {
+                throw new \Exception(
+                    "Cannot access LocalStorage components with incorrect security key"
+                );
+            }
+        }
+
         $uncompress = @gzinflate($encoding);
+
         if($uncompress !== false) {
             return
                 unserialize(
@@ -76,6 +113,7 @@ abstract class AbstractLocalStorage implements LocalStorageInterface
                 )
             ;
         }
+
         return [];
     }
 }
