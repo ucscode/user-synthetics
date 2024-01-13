f9b63172d239511f72c64068bf73558b133e7875
Ucscode <uche23mail@gmail.com>
2024-01-14 00:05:15 +0100

    Chore: fixed require_once on undefined path

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ define('INSTALLATION_PATH', str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT_DIR)
 call_user_func(function () {
     $autoloader = __DIR__ . "/vendor/autoload.php";
     if (!is_file($autoloader)) {
-        die(require_once __DIR__ . "/uss-core/help.php");
+        die(require_once __DIR__ . "/cores/help.php");
     }
     require_once $autoloader;
 });
