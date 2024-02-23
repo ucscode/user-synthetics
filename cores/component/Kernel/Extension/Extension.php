08f80defd53228411e7fdd049ac373ababba69b8
ucscode <uche23mail@gmail.com>
2024-02-23 14:04:47 +0100

    feat: enabled 'replaceUrlQuery' method call within twig template'

--- cores/component/Kernel/Extension/Extension.php
+++ cores/component/Kernel/Extension/Extension.php
@@ -81,6 +81,7 @@ final class Extension extends AbstractExtension implements ExtensionInterface, G
                 'getTemplateSchema',
                 'relativeTime',
                 'arrayToHtmlAttrs',
+                'replaceUrlQuery'
             ],
             $this->framework
         );
