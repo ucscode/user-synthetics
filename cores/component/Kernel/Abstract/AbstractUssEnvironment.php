c63952382a86e7b333197d004d85580bca68eead
ucscode <uche23mail@gmail.com>
2024-02-27 06:39:47 +0100

    refactor: page_icon -> page_logo

--- cores/component/Kernel/Abstract/AbstractUssEnvironment.php
+++ cores/component/Kernel/Abstract/AbstractUssEnvironment.php
@@ -34,7 +34,7 @@ abstract class AbstractUssEnvironment implements UssFrameworkInterface
         return [
             'html_language' => 'en',
             'page_title' => UssImmutable::PROJECT_NAME,
-            'page_icon' => $this->pathToUrl(UssImmutable::ASSETS_DIR . '/images/origin.png', false),
+            'page_logo' => $this->pathToUrl(UssImmutable::ASSETS_DIR . '/images/origin.png', false),
             'page_slogan' => 'A Modular PHP Framework for Building Customized Web Applications',
             'page_description' => "User Synthetics is a powerful and versatile PHP framework designed to simplify the development of customizable and extensible web applications.",
         ];
